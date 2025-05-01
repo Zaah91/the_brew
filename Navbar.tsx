0bfd28ec300c0b3a6a3eb7b18863d7ebd10bdee2
David Praëm Andreassen <124776389+BPraem@users.noreply.github.com>
2025-05-01 17:08:01 +0200

    UX - Changes

---
+++ Navbar.tsx
@@ -0,0 +1,15 @@
+'use client';
+import Link from 'next/link';
+
+const Navbar = () => {
+  return (
+    <nav className="flex items-center px-10 py-4 bg-[#FAF0E6] text-[20px]">
+      <Link href="/" className="mr-4 text-black">Home</Link>
+      <Link href="/shop" className="mr-4 text-black">Shop</Link>
+      {/* <Link href="/nonAlcohol" className="mr-4 text-black">Non alcoholic</Link> */}
+      <Link href="/checkout" className="text-black ml-auto">Cart</Link>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ ProductInfoCard.tsx
@@ -0,0 +1,39 @@
+'use client';
+
+import Image from 'next/image';
+import { Data } from '@/utils/interfaces';
+import { useRouter } from 'next/navigation'; // Import useRouter from next/navigation
+
+export default function DrinkCard({ product }: { product: Data }) {
+  const router = useRouter(); // Initialize the router
+  // console.log('Product in DrinkCard:', product); // Log the product for debugging
+
+  return (
+    <div className="p-4 shadow-md hover:shadow-lg transition-shadow">
+      <button
+        className="w-60 bg-gray-500 text-white py-2 px-4 rounded hover:bg-gray-700 focus:outline-2 focus:outline-offset-2 focus:outline-gray-900 active:bg-gray-800 transition-colors mb-2"
+        onClick={() => router.back()}
+      >
+        Go back
+      </button>
+
+      <div className="flex items-start gap-4 items-center justify-center text-center mt-16">
+        <div className="flex-shrink-0">
+          <Image
+            src={product.image.url || '/duck.webp'}
+            alt={product.name}
+            width={200}
+            height={200}
+            className="w-120 h-120 object-cover rounded-md"
+          />
+        </div>
+        <div>
+          <h3 className="font-bold text-2xl mb-2">{product.name}</h3>
+          <p className="text-gray-100 mb-4 max-w-prose mb-4">{product.description}</p>
+          <p className="text-base text-gray-200 mb-4">Category: {product.category.name}</p>
+          <p className="text-base text-gray-200 mb-4">Price: ${product.price}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
